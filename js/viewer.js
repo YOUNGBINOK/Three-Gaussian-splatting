fb220390f342258efda35e0e5bd8dd62294690a8
ok <redmangos@naver.com>
2024-08-22 19:12:37 +0900

    feat: mobile

--- js/viewer.js
+++ js/viewer.js
@@ -2,9 +2,33 @@ import * as GaussianSplats3D from "@mkkellogg/gaussian-splats-3d";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import nipplejs from "nipplejs";
+
+function detectMobileDevice(agent) {
+  const mobileRegex = [/Android/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
+
+  return mobileRegex.some((mobile) => agent.match(mobile));
+}
+
+const isMobile = detectMobileDevice(window.navigator.userAgent);
+
+if (isMobile) {
+  console.log("current device is mobile");
+} else {
+  console.log("current device is not mobile");
+}
 
 let model, path, mixer, actions, idleAction, walkAction, step, yaw;
 let modelPath = "/assets/models/jogging_woman.glb";
+
+let fwdValue = 0;
+let bkdValue = 0;
+let rgtValue = 0;
+let lftValue = 0;
+let tempVector = new THREE.Vector3();
+let upVector = new THREE.Vector3(0, 1, 0);
+let joyManager;
+
 let clock = new THREE.Clock();
 
 const UIButton = document.getElementById("liveToastBtn");
@@ -48,7 +72,7 @@ async function viewer(scene) {
     isLocked = document.pointerLockElement === rootElement;
   });
 
-  window.addEventListener("keydown", function (event) {
+  document.addEventListener("keydown", function (event) {
     if (["KeyW", "KeyA", "KeyS", "KeyD"].includes(event.code)) {
       event.preventDefault();
       keys[event.code.toLowerCase().slice(-1)] = true;
@@ -56,12 +80,50 @@ async function viewer(scene) {
     }
   });
 
-  window.addEventListener("keyup", function (event) {
+  document.addEventListener("keyup", function (event) {
     if (["KeyW", "KeyA", "KeyS", "KeyD"].includes(event.code)) {
       event.preventDefault();
       keys[event.code.toLowerCase().slice(-1)] = false;
       if (keys.a === false && keys.w === false && keys.s === false && keys.d === false) isWalking = false;
     }
+  });
+
+  document.addEventListener("touchend", () => {
+    isLocked = false;
+  });
+
+  document.addEventListener("touchstart", (event) => {
+    const initialTouchX = event.touches[0].clientX;
+    //const initialTouchY = event.touches[0].clientY;
+
+    document.addEventListener(
+      "touchmove",
+      (event) => {
+        const currentTouchX = event.touches[0].clientX;
+        //const currentTouchY = event.touches[0].clientY;
+
+        const deltaX = currentTouchX - initialTouchX;
+        //const deltaY = currentTouchY - initialTouchY;
+
+        console.log("Movement:", deltaX);
+        yaw -= deltaX * 0.0002;
+      },
+      { passive: false }
+    ); // Prevent default scrolling behavior
+  });
+
+  document.addEventListener("mousedown", (event) => {
+    isLocked = true;
+    document.addEventListener("mousemove", (event) => {
+      if (isLocked) {
+        console.log(event);
+        yaw -= event.movementX * 0.0002;
+      }
+    });
+  });
+
+  document.addEventListener("mouseup", (event) => {
+    isLocked = false;
   });
 
   const light = new THREE.AmbientLight("#ffffff");
@@ -81,7 +143,7 @@ async function viewer(scene) {
     case "bicycle":
       step = 0.05;
       yaw = -Math.PI / 1.1;
-      viewer.splatMesh.rotation.set(THREE.MathUtils.degToRad(-15), 0, 0);
+      viewer.splatobject.rotation.set(THREE.MathUtils.degToRad(-15), 0, 0);
       params = {
         ...params,
         cameraUp: [0.07552, -0.9436, -0.32235],
@@ -99,7 +161,7 @@ async function viewer(scene) {
     case "garden":
       step = 0.05;
       yaw = -Math.PI / 3;
-      viewer.splatMesh.rotation.set(THREE.MathUtils.degToRad(-30), 0, 0);
+      viewer.splatobject.rotation.set(THREE.MathUtils.degToRad(-30), 0, 0);
       params = {
         ...params,
         cameraUp: [0.0, -1.0, 0.0],
@@ -117,7 +179,7 @@ async function viewer(scene) {
     case "stump":
       step = 0.09;
       yaw = Math.PI / 3;
-      viewer.splatMesh.rotation.set(THREE.MathUtils.degToRad(-46.5), 0, 0);
+      viewer.splatobject.rotation.set(THREE.MathUtils.degToRad(-46.5), 0, 0);
       params = {
         ...params,
         cameraUp: [0.0, -1.70711, -0.70711],
@@ -133,7 +195,7 @@ async function viewer(scene) {
       );
       break;
     case "postshot":
-      step = 0.05;
+      step = 0.04;
       yaw = Math.PI / 1.8;
       params = {
         ...params,
@@ -152,7 +214,7 @@ async function viewer(scene) {
     case "treehill":
       yaw = -Math.PI / 1.1;
       step = 0.04;
-      viewer.splatMesh.rotation.set(THREE.MathUtils.degToRad(-25), 0, 0);
+      viewer.splatobject.rotation.set(THREE.MathUtils.degToRad(-25), 0, 0);
       params = {
         ...params,
         cameraUp: [-0.15356, -0.97605, -0.15408],
@@ -201,19 +263,6 @@ async function viewer(scene) {
       console.error("Error loading file:", error);
     });
 
-  document.addEventListener("mousedown", (event) => {
-    isLocked = true;
-    document.addEventListener("mousemove", (event) => {
-      if (isLocked) {
-        yaw -= event.movementX * 0.0002;
-      }
-    });
-  });
-
-  document.addEventListener("mouseup", (event) => {
-    isLocked = false;
-  });
-
   // 카메라 업데이트
   const offset = new THREE.Vector3(0, 0.19, 0.4);
   async function updateCameraPosition() {
@@ -228,10 +277,46 @@ async function viewer(scene) {
 
       const moveDirection = new THREE.Vector3();
 
-      if (keys.w) moveDirection.add(direction.clone().multiplyScalar(step));
-      if (keys.s) moveDirection.add(direction.clone().negate().multiplyScalar(step));
-      if (keys.a) moveDirection.add(sideDirection.clone().negate().multiplyScalar(step));
-      if (keys.d) moveDirection.add(sideDirection.clone().multiplyScalar(step));
+      if (isMobile) {
+        // 모바일 환경 일 때
+        if (fwdValue > 0) {
+          isWalking = true;
+          tempVector = moveDirection.add(direction.clone().multiplyScalar(step));
+          model.scene.position.addScaledVector(tempVector, 1);
+        } else {
+          isWalking = false;
+        }
+
+        if (bkdValue > 0) {
+          isWalking = true;
+          tempVector = moveDirection.add(direction.clone().negate().multiplyScalar(step));
+          model.scene.position.addScaledVector(tempVector, 1);
+        } else {
+          isWalking = false;
+        }
+
+        if (lftValue > 0) {
+          isWalking = true;
+          tempVector = moveDirection.add(sideDirection.clone().negate().multiplyScalar(step));
+          model.scene.position.addScaledVector(tempVector, 1);
+        } else {
+          isWalking = false;
+        }
+
+        if (rgtValue > 0) {
+          isWalking = true;
+          tempVector = moveDirection.add(sideDirection.clone().multiplyScalar(step));
+          model.scene.position.addScaledVector(tempVector, 1);
+        } else {
+          isWalking = false;
+        }
+      } else {
+        // 모바일 환경이 아닐 떄
+        if (keys.w) moveDirection.add(direction.clone().multiplyScalar(step));
+        if (keys.s) moveDirection.add(direction.clone().negate().multiplyScalar(step));
+        if (keys.a) moveDirection.add(sideDirection.clone().negate().multiplyScalar(step));
+        if (keys.d) moveDirection.add(sideDirection.clone().multiplyScalar(step));
+      }
 
       model.scene.position.add(moveDirection);
       const newCameraPosition = model.scene.position
@@ -261,6 +346,7 @@ async function viewer(scene) {
   }
 
   animate();
+  addJoystick();
 }
 
 async function loadGltf(scene, path, position, lookAt) {
@@ -280,11 +366,53 @@ async function loadGltf(scene, path, position, lookAt) {
       idleAction.play();
       scene.add(gltf.scene);
     },
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
+    function (xhr) {},
     function (error) {
       console.log("An error happened");
     }
   );
+}
+
+function addJoystick() {
+  const options = {
+    zone: document.getElementById("joystickWrapper1"),
+    size: 120,
+    multitouch: true,
+    maxNumberOfNipples: 2,
+    mode: "static",
+    restJoystick: true,
+    shape: "circle",
+    position: { top: "60px", left: "60px" },
+    dynamicPage: true,
+  };
+
+  joyManager = nipplejs.create(options);
+
+  joyManager["0"].on("move", function (evt, data) {
+    const forward = data.vector.y;
+    const turn = data.vector.x;
+
+    if (forward > 0) {
+      fwdValue = Math.abs(forward);
+      bkdValue = 0;
+    } else if (forward < 0) {
+      fwdValue = 0;
+      bkdValue = Math.abs(forward);
+    }
+
+    if (turn > 0) {
+      lftValue = 0;
+      rgtValue = Math.abs(turn);
+    } else if (turn < 0) {
+      lftValue = Math.abs(turn);
+      rgtValue = 0;
+    }
+  });
+
+  joyManager["0"].on("end", function (evt) {
+    bkdValue = 0;
+    fwdValue = 0;
+    lftValue = 0;
+    rgtValue = 0;
+  });
 }
